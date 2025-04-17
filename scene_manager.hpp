fe9a8f7829b71d394d434333c52447dd707a7c18
Hagai Badichi <hagaybadichi@gmail.com>
2025-04-17 20:15:03 +0300

    feat: added scene_manager

---
+++ scene_manager.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "scenes/iscene.h"
+#include "scenes/test_scene.h"
+
+class scene_manager
+{
+public:
+    scene_manager() = default;
+
+    template <class T, class ... Args>
+    void set_scene(Args&&... args);
+
+    [[nodiscard]] iscene* active_scene() const;
+
+private:
+    std::unique_ptr<iscene> m_scene = nullptr;
+};
+
+iscene* scene_manager::active_scene() const
+{
+    return m_scene.get();
+}
+
+template <typename T, typename ... Args>
+void scene_manager::set_scene(Args&&... args)
+{
+    if (m_scene != nullptr)
+    {
+        m_scene->exit();
+    }
+
+    m_scene = std::make_unique<T>(std::forward<Args>(args)...);
+
+    m_scene->start();
+}
+
+
+extern scene_manager g_scene_manager;
