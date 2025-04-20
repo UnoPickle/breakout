ae29a8a9888c3dc4c71be7c510eace2a78cd8ff8
Hagai Badichi <hagaybadichi@gmail.com>
2025-04-20 14:16:35 +0300

    feat: added input to nav

--- scenes/game_over_scene.cpp
+++ scenes/game_over_scene.cpp
@@ -1,48 +1,59 @@
 #include "game_over_scene.h"
 
+#include "breakout_scene.h"
 #include "../breakout_defs.h"
+#include "../input.h"
+#include "../scene_manager.hpp"
 #include "../resources/font_resource.h"
 #include "../resources/resource_manager.h"
 
 game_over_scene::game_over_scene(const uint64_t score) : m_title(
                                                              static_cast<font_resource*>(g_resource_manager.
                                                                  get_resource("assets/Jersey10-Regular.ttf"))->get_font(
-                                                                 SCORE_TEXT_SIZE),
-                                                             "Your score:", SCORE_TEXT_COLOR, {0, 0}),
+                                                                 TITLE_TEXT_SIZE),
+                                                             "Your score:", SCENE_TEXT_COLOR, {0, 0}),
                                                          m_score(
                                                              static_cast<font_resource*>(g_resource_manager.
                                                                  get_resource("assets/Jersey10-Regular.ttf"))->get_font(
                                                                  SCORE_TEXT_SIZE),
-                                                             std::to_string(score), SCORE_TEXT_COLOR, {0, 0}),
+                                                             std::to_string(score), SCENE_TEXT_COLOR, {0, 0}),
                                                          m_navigation_restart(
                                                              static_cast<font_resource*>(g_resource_manager.
                                                                  get_resource("assets/Jersey10-Regular.ttf"))->get_font(
-                                                                 SCORE_TEXT_SIZE),
-                                                             "Press 'R' to restart", SCORE_TEXT_COLOR, {0, 0}),
+                                                                 NAV_TEXT_SIZE),
+                                                             "Press '"  + std::string(SDL_GetKeyName(SDL_GetKeyFromScancode(RESTART_SCANCODE, SDL_KMOD_NONE, false))) + "' to restart", SCENE_TEXT_COLOR, {0, 0}),
                                                          m_navigation_menu(
                                                              static_cast<font_resource*>(g_resource_manager.
                                                                  get_resource("assets/Jersey10-Regular.ttf"))->get_font(
-                                                                 SCORE_TEXT_SIZE),
-                                                             "Press 'ESC' for menu", SCORE_TEXT_COLOR, {0, 0})
+                                                                 NAV_TEXT_SIZE),
+                                                             "Press '"  + std::string(SDL_GetKeyName(SDL_GetKeyFromScancode(MENU_SCANCODE, SDL_KMOD_NONE, false))) + "' for menu", SCENE_TEXT_COLOR, {0, 0})
 
 {
     m_score.set_pos({
         static_cast<float>(breakout_defs::WINDOW_WIDTH) / 2 - m_score.get_surf().surface_object()->w / 2,
-        static_cast<float>(breakout_defs::WINDOW_HEIGHT) / 2 - m_score.get_surf().surface_object()->h / 2
+        static_cast<float>(breakout_defs::WINDOW_HEIGHT) / 2 - m_score.get_surf().surface_object()->h / 2 - BOTTOM_MARGIN
     });
 
     m_title.set_pos({
         static_cast<float>(breakout_defs::WINDOW_WIDTH) / 2 - m_title.get_surf().surface_object()->w / 2,
-        m_score.get_pos().y - m_title.get_surf().surface_object()->h / 2 - SCORE_TITLE_MARGIN
+        m_score.get_pos().y - m_score.get_surf().surface_object()->h / 2 - m_title.get_surf().surface_object()->h / 2 - SCORE_TITLE_MARGIN
     });
 
-    /*m_navigation_restart.set_pos({
+    m_navigation_restart.set_pos({
         static_cast<float>(breakout_defs::WINDOW_WIDTH) / 2 - m_navigation_restart.get_surf().surface_object()->w / 2,
-        static_cast<float>(breakout_defs::WINDOW_HEIGHT) / 2 - m_score.get_surf().surface_object()->h / 2
-    });*/
+        m_score.get_pos().y + m_score.get_surf().surface_object()->h / 2 + m_navigation_restart.get_surf().surface_object()->h / 2 + SCORE_TITLE_MARGIN
+    });
+
+    m_navigation_menu.set_pos({
+        static_cast<float>(breakout_defs::WINDOW_WIDTH) / 2 - m_navigation_menu.get_surf().surface_object()->w / 2,
+        m_navigation_restart.get_pos().y + m_navigation_menu.get_surf().surface_object()->h / 2 + RESTART_MENU_MARGIN
+    });
 
     add_object(m_title);
     add_object(m_score);
+
+    add_object(m_navigation_restart);
+    add_object(m_navigation_menu);
 }
 
 void game_over_scene::start()
@@ -51,6 +62,15 @@ void game_over_scene::start()
 
 void game_over_scene::update(double deltatime)
 {
+    if (g_input.get_key(RESTART_SCANCODE) == key_state::DOWN)
+    {
+        g_scene_manager.set_scene<breakout_scene>();
+    }
+
+    if (g_input.get_key(MENU_SCANCODE) == key_state::DOWN)
+    {
+        g_scene_manager.set_scene<test_scene>();
+    }
 }
 
 void game_over_scene::exit()
--- scenes/game_over_scene.h
+++ scenes/game_over_scene.h
@@ -3,11 +3,21 @@
 #include "../vo/text_obj.h"
 
 constexpr float SCORE_TEXT_SIZE = 80.0f;
-constexpr SDL_Color SCORE_TEXT_COLOR = {0xff, 0xff, 0xff, 0};
+constexpr float TITLE_TEXT_SIZE = 80.0f;
+constexpr float NAV_TEXT_SIZE = 60.0f;
+
+constexpr SDL_Color SCENE_TEXT_COLOR = {0xff, 0xff, 0xff, 0};
 
 constexpr float SCORE_TITLE_MARGIN = 10.0f;
-
 constexpr float SCORE_NAV_MARGIN = 20.0f;
+
+constexpr float RESTART_MENU_MARGIN = 10.0f;
+
+constexpr float BOTTOM_MARGIN = 30.0f;
+
+
+constexpr SDL_Scancode RESTART_SCANCODE = SDL_SCANCODE_R;
+constexpr SDL_Scancode MENU_SCANCODE = SDL_SCANCODE_ESCAPE;
 
 class game_over_scene : public iscene
 {
